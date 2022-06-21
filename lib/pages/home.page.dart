b5e44500427b14408728145b8f56eedfd23c0ad3
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-06-21 17:58:12 -0300

    feat: created product listing

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -14,6 +14,47 @@ class HomePage extends StatelessWidget {
               height: 60,
             ),
             search(),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Categories",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 90,
+              child: categoryList(),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Best Selling",
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                FlatButton(
+                  child: Text("See All"),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 350,
+              child: productList(),
+            ),
           ],
         ),
       ),
@@ -54,6 +95,115 @@ Widget search() {
               fontSize: 20,
               color: Colors.blue,
             ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget categoryList() {
+  return Container(
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+      ],
+    ),
+  );
+}
+
+Widget categoryItem() {
+  return Container(
+      width: 70,
+      height: 70,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          new BoxShadow(
+            color: Colors.black12,
+            offset: new Offset(1, 1),
+            blurRadius: 5,
+            spreadRadius: 2,
+          ),
+        ],
+        borderRadius: BorderRadius.all(
+          Radius.circular(64),
+        ),
+      ),
+      child: Image.asset("assets/Icon_Devices.png"));
+}
+
+Widget productList() {
+  return Container(
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+      ],
+    ),
+  );
+}
+
+Widget productItem() {
+  return Container(
+    padding: EdgeInsets.all(10),
+    margin: EdgeInsets.all(5),
+    width: 170,
+    color: Colors.black12,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Image.asset(
+          "assets/product-1.png",
+          width: 170,
+          height: 170,
+          fit: BoxFit.cover,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 60,
+          child: Text(
+            "Titulo do produto",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          "Marca",
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          "\$ 200",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF00C569),
           ),
         ),
       ],
