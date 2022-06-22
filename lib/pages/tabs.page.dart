94a46c47cfebac9fd64f810fc912df56308b291f
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-06-21 23:01:24 -0300

    feat: created tabs page

---
+++ lib/pages/tabs.page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'cart.page.dart';
+import 'home.page.dart';
+import 'login.page.dart';
+
+class TabsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        children: [
+          HomePage(),
+          CartPage(),
+          LoginPage(),
+        ],
+      ),
+      bottomNavigationBar: new TabBar(
+        tabs: [
+          Tab(
+            icon: new Icon(Icons.home),
+          ),
+          Tab(
+            icon: new Icon(Icons.shopping_cart),
+          ),
+          Tab(
+            icon: new Icon(Icons.perm_identity),
+          ),
+        ],
+        labelColor: Colors.blue,
+        unselectedLabelColor: Colors.black38,
+        indicatorSize: TabBarIndicatorSize.label,
+        indicatorPadding: EdgeInsets.all(5.0),
+        indicatorColor: Colors.blue,
+      ),
+    );
+  }
+}
