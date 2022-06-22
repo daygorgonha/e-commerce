7d2999a71c6fd3f6d88bf178024ebdec44f3d01d
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-06-21 23:02:34 -0300

    feat: created navigation between pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:layouts_no_flutter/pages/login.page.dart';
-
-import 'pages/cart.page.dart';
+import 'pages/tabs.page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -16,7 +14,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: CartPage(),
+      home: DefaultTabController(
+        length: 3,
+        child: TabsPage(),
+      ),
     );
   }
 }
